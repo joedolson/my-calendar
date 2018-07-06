3dd9f2e958c53185a75fa553338cc2c56c709d16
Joe Dolson <design@joedolson.com>
2018-07-06 11:20:42 -0500

    Update version numbers

--- src/my-calendar.php
+++ src/my-calendar.php
@@ -17,7 +17,7 @@
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/license/gpl-2.0.txt
  * Domain Path: lang
- * Version:     3.0.9
+ * Version:     3.0.10
  */
 
 /*
@@ -42,7 +42,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 global $mc_version, $wpdb;
-$mc_version = '3.0.9';
+$mc_version = '3.0.10';
 
 define( 'MC_DEBUG', false );
 
