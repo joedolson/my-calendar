f5d234117897f9a0ab55132369a7d67237c4dcf2
Joe Dolson <design@joedolson.com>
2018-11-13 19:09:47 -0600

    Formatting

--- src/my-calendar-core.php
+++ src/my-calendar-core.php
@@ -1411,10 +1411,10 @@ $plugins_string
 			if ( ! wp_verify_nonce( $nonce, 'my-calendar-nonce' ) ) {
 				wp_die( 'Security check failed' );
 			}
-			$request       = ( ! empty( $_POST['support_request'] ) ) ? stripslashes( $_POST['support_request'] ) : false;
-			$has_read_faq  = ( 'on' == $_POST['has_read_faq'] ) ? 'Read FAQ' : false;
-			$subject       = "My Calendar support request.";
-			$message       = $request . "\n\n" . $data;
+			$request      = ( ! empty( $_POST['support_request'] ) ) ? stripslashes( $_POST['support_request'] ) : false;
+			$has_read_faq = ( 'on' == $_POST['has_read_faq'] ) ? 'Read FAQ' : false;
+			$subject      = 'My Calendar Pro support request.';
+			$message      = $request . "\n\n" . $data;
 			// Get the site domain and get rid of www. from pluggable.php.
 			$sitename = strtolower( $_SERVER['SERVER_NAME'] );
 			if ( 'www.' == substr( $sitename, 0, 4 ) ) {
