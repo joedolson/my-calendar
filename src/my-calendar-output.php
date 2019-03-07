d64e36a9514c26bdf10320cff8e0f52330666f24
Joe Dolson <design@joedolson.com>
2019-03-07 12:51:55 -0600

    Disable details if no_link toggled

--- src/my-calendar-output.php
+++ src/my-calendar-output.php
@@ -636,7 +636,9 @@ function mc_category_class( $object, $prefix ) {
  * @return boolean
  */
 function mc_show_details( $time, $type ) {
-	return ( 'calendar' == $type && 'true' == get_option( 'mc_open_uri' ) && 'day' != $time ) ? false : true;
+	$no_link = apply_filters( 'mc_disable_link', false, $data );
+
+	return ( ( 'calendar' == $type && 'true' == get_option( 'mc_open_uri' ) && 'day' != $time ) || $no_link ) ? false : true;
 }
 
 add_filter( 'mc_after_event', 'mc_edit_panel', 10, 4 );
