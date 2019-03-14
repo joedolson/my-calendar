c35d06f8c1e8734b14802ff9a9755e5646da48e2
Joe Dolson <design@joedolson.com>
2019-03-14 15:10:08 -0500

    Move sortable behaviors together

--- src/js/jquery.admin.js
+++ src/js/jquery.admin.js
@@ -92,6 +92,24 @@ jQuery(document).ready(function ($) {
 			$( '#mc-generator .custom' ).hide();
 		}
 	});
+
+	$('#mc-sortable').sortable({
+		update: function (event, ui) {
+			$('#mc-sortable-update').html( 'Submit form to save changes' );
+		}
+	});
+	$('#mc-sortable .up').on('click', function (e) {
+		e.preventDefault();
+		$(this).parents('li').insertBefore($(this).parents('li').prev());
+		$( '#mc-sortable li' ).removeClass( 'mc-updated' );
+		$(this).parents('li').addClass( 'mc-updated' );
+	});
+	$('#mc-sortable .down').on('click', function (e) {
+		e.preventDefault();
+		$(this).parents('li').insertAfter($(this).parents('li').next());
+		$( '#mc-sortable li' ).removeClass( 'mc-updated' );
+		$(this).parents('li').addClass( 'mc-updated' );
+	});
 });
 
 var mediaPopup = '';
@@ -148,23 +166,3 @@ var mediaPopup = '';
 			})
 	});
 })(jQuery);
-
-jQuery(document).ready(function ($) {
-	$('#mc-sortable').sortable({
-		update: function (event, ui) {
-			$('#mc-sortable-update').html( 'Submit form to save changes' );
-		}
-	});
-	$('#mc-sortable .up').on('click', function (e) {
-		e.preventDefault();
-		$(this).parents('li').insertBefore($(this).parents('li').prev());
-		$( '#mc-sortable li' ).removeClass( 'mc-updated' );
-		$(this).parents('li').addClass( 'mc-updated' );
-	});
-	$('#mc-sortable .down').on('click', function (e) {
-		e.preventDefault();
-		$(this).parents('li').insertAfter($(this).parents('li').next());
-		$( '#mc-sortable li' ).removeClass( 'mc-updated' );
-		$(this).parents('li').addClass( 'mc-updated' );
-	});
-});
