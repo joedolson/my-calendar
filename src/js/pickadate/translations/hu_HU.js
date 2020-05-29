e203f845476798bfeca135fe777605a268ec777c
Joe Dolson <design@joedolson.com>
2020-05-29 15:06:45 -0500

    Update to v. 3.6.4 of Pickadate library

--- src/js/pickadate/translations/hu_HU.js
+++ src/js/pickadate/translations/hu_HU.js
@@ -1,11 +1,18 @@
-jQuery.extend(jQuery.fn.pickadate.defaults, {
-    monthsFull: ["január", "február", "március", "aprilis", "május", "június", "július", "augusztus", "szeptember", "október", "november", "december"],
-    monthsShort: ["jan", "febr", "márc", "apr", "máj", "jún", "júl", "aug", "szept", "okt", "nov", "dec"],
-    weekdaysFull: ["vasámap", "hétfö", "kedd", "szerda", "csütörtök", "péntek", "szombat"],
-    weekdaysShort: ["V", "H", "K", "SZ", "CS", "P", "SZ"],
-    today: "ma",
-    clear: "töröl",
+// Hungarian
+
+jQuery.extend( jQuery.fn.pickadate.defaults, {
+    monthsFull: [ 'január', 'február', 'március', 'április', 'május', 'június', 'július', 'augusztus', 'szeptember', 'október', 'november', 'december' ],
+    monthsShort: [ 'jan', 'febr', 'márc', 'ápr', 'máj', 'jún', 'júl', 'aug', 'szept', 'okt', 'nov', 'dec' ],
+    weekdaysFull: [ 'vasárnap', 'hétfő', 'kedd', 'szerda', 'csütörtök', 'péntek', 'szombat' ],
+    weekdaysShort: [ 'V', 'H', 'K', 'Sze', 'CS', 'P', 'Szo' ],
+    today: 'Ma',
+    clear: 'Törlés',
+    close: 'Bezárás',
     firstDay: 1,
-    format: "yyyy. mmmm dd.",
-    formatSubmit: "yyyy/mm/dd"
+    format: 'yyyy. mmmm dd.',
+    formatSubmit: 'yyyy/mm/dd'
+});
+
+jQuery.extend( jQuery.fn.pickatime.defaults, {
+    clear: 'Törlés'
 });
