ff00b14eb48a9bbc102270dcaf37ef0ce1943044
Joe Dolson <design@joedolson.com>
2020-06-26 11:46:12 -0500

    Casting as category without prior type checking
    Doing it this way forced a match anytime the first if statement doesn't
    matches.

--- src/my-calendar-categories.php
+++ src/my-calendar-categories.php
@@ -817,7 +817,7 @@ function mc_category_select( $data = false, $option = true, $multiple = false, $
 				if ( $multiple ) {
 					if ( is_array( $category ) && in_array( $cat->category_id, $category, true ) ) {
 						$selected = ' checked="checked"';
-					} elseif ( (int) $category === (int) $cat->category_id ) {
+					} elseif ( is_numeric( $category ) && ( (int) $category === (int) $cat->category_id ) ) {
 						$selected = ' checked="checked"';
 					}
 				} else {
