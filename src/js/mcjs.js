b16b1914d97223f92a5554f37cbbcaa04ee5e16a
Joe Dolson <design@joedolson.com>
2020-10-05 12:08:11 -0500

    Extra spaces

--- src/js/mcjs.js
+++ src/js/mcjs.js
@@ -3,7 +3,6 @@
 	$(function () { 
 		$( '.mc-main' ).removeClass( 'mcjs' ); 
 	});
-	
-	$('.mc-main a[target=_blank]').append( ' <span class="dashicons dashicons-external" aria-hidden="true"></span><span class="screen-reader-text"> ' + my_calendar.newWindow + '</span>' );
 
+	$('.mc-main a[target=_blank]').append( ' <span class="dashicons dashicons-external" aria-hidden="true"></span><span class="screen-reader-text"> ' + my_calendar.newWindow + '</span>' );
 }(jQuery));
