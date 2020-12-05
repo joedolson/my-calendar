20747f6fa8fedc2eba0e701780cf704a88140e9e
Joe Dolson <design@joedolson.com>
2020-12-05 09:41:58 -0600

    Message name needs to be passed in the right function.

--- src/my-calendar-event-manager.php
+++ src/my-calendar-event-manager.php
@@ -694,7 +694,7 @@ function my_calendar_save( $action, $output, $event_id = false ) {
 				my_calendar_send_email( $event );
 			}
 			if ( '0' === (string) $add['event_approved'] ) {
-				$message = mc_show_notice( __( 'Event draft saved.', 'my-calendar', 'draft-saved' ), false );
+				$message = mc_show_notice( __( 'Event draft saved.', 'my-calendar' ), false, 'draft-saved' );
 			} else {
 				// jd_doTwitterAPIPost was changed to wpt_post_to_twitter on 1.19.2017.
 				if ( function_exists( 'wpt_post_to_twitter' ) && isset( $_POST['mc_twitter'] ) && '' !== trim( $_POST['mc_twitter'] ) ) {
@@ -768,7 +768,7 @@ function my_calendar_save( $action, $output, $event_id = false ) {
 					} else {
 						// Only dates were changed.
 						$result  = mc_update_instance( $event_instance, $event_id, $update );
-						$message = mc_show_notice( __( 'Date/time information for this event has been updated.', 'my-calendar', 'date-updated' ) . " $url", false );
+						$message = mc_show_notice( __( 'Date/time information for this event has been updated.', 'my-calendar' ) . " $url", false, 'date-updated' );
 					}
 				}
 			} else {
@@ -812,7 +812,7 @@ function my_calendar_save( $action, $output, $event_id = false ) {
 					// Don't execute transition actions if prev status not known.
 					do_action( 'mc_transition_event', (int) $_POST['prev_event_status'], $event_approved, $action, $data, $event_id );
 				}
-				$message = mc_show_notice( __( 'Event updated successfully', 'my-calendar', 'event-updated' ) . ". $url", false );
+				$message = mc_show_notice( __( 'Event updated successfully', 'my-calendar' ) . ". $url", false, 'event-updated' );
 			}
 		} else {
 			$message = mc_show_error( __( 'You do not have sufficient permissions to edit that event.', 'my-calendar' ), false );
@@ -936,7 +936,7 @@ function mc_delete_event( $event_id ) {
 			} else {
 				do_action( 'mc_delete_event', $event_id, $post_id );
 			}
-			$message = mc_show_notice( __( 'Event deleted successfully', 'my-calendar', 'event-deleted' ), false );
+			$message = mc_show_notice( __( 'Event deleted successfully', 'my-calendar' ), false, 'event-deleted' );
 		} else {
 			$message = mc_show_error( __( 'Despite issuing a request to delete, the event still remains in the database. Please investigate.', 'my-calendar' ), false );
 		}
