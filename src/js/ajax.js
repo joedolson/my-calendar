1637aa39e3ee8615b384e307f573ea5b0d211c18
Joe Dolson <design@joedolson.com>
2021-03-28 14:29:25 -0500

    Change toggles to use classes

--- src/js/ajax.js
+++ src/js/ajax.js
@@ -28,9 +28,9 @@
 		$( 'button.add-occurrence').on( 'click', function() {
 			var expanded = $( this ).attr( 'aria-expanded' );
 			if ( expanded == 'true' ) {
-				$( this ).attr( 'aria-expanded', 'false' );
+				$( this ).attr( 'aria-expanded', 'false' ).addClass( 'dashicons-minus' ).removeClass( 'dashicons-plus' );
 			} else {
-				$( this ).attr( 'aria-expanded', 'true' );
+				$( this ).attr( 'aria-expanded', 'true' ).addClass( 'dashicons-plus' ).removeClass( 'dashicons-minus' );
 			}
 			$( '.mc_add_new' ).toggle();
 		});
