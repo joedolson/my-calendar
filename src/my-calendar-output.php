3ed152e858092c09372309652bcc312d29cbe7b8
Joe Dolson <design@joedolson.com>
2021-09-01 18:05:35 -0500

    add opl-link class (smoothscroll)
    Prevents Ocean WP smoothscroll from breaking links.

--- src/my-calendar-output.php
+++ src/my-calendar-output.php
@@ -381,7 +381,7 @@ function my_calendar_draw_event( $event, $type = 'calendar', $process_date, $tim
 			$wrap         = ( _mc_is_url( $details_link ) ) ? "<a href='$details_link' class='url summary$has_image' $nofollow>" : '<span class="no-link">';
 			$balance      = ( _mc_is_url( $details_link ) ) ? '</a>' : '</span>';
 		} else {
-			$wrap    = "<a href='#$uid-$type-details-$id' class='et_smooth_scroll_disabled url summary$has_image'>";
+			$wrap    = "<a href='#$uid-$type-details-$id' class='et_smooth_scroll_disabled opl-link url summary$has_image'>";
 			$balance = '</a>';
 		}
 	} else {
