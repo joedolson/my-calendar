c120241d55ad5306d3c052d9061ad75af8ea5af6
Joe Dolson <design@joedolson.com>
2021-10-11 15:26:04 -0500

    Remove custom JS fields.

--- src/my-calendar-behaviors.php
+++ src/my-calendar-behaviors.php
@@ -29,28 +29,12 @@ function my_calendar_behaviors_edit() {
 		update_option( 'mc_list_javascript', ( empty( $_POST['list_js'] ) ) ? 0 : 1 );
 		update_option( 'mc_mini_javascript', ( empty( $_POST['mini_js'] ) ) ? 0 : 1 );
 		update_option( 'mc_ajax_javascript', ( empty( $_POST['ajax_js'] ) ) ? 0 : 1 );
-		// set js.
-		if ( isset( $_POST['mc_caljs'] ) ) {
-			$mc_caljs  = $_POST['mc_caljs'];
-			$mc_listjs = $_POST['mc_listjs'];
-			$mc_minijs = $_POST['mc_minijs'];
-			$mc_ajaxjs = $_POST['mc_ajaxjs'];
-			update_option( 'mc_listjs', $mc_listjs );
-			update_option( 'mc_minijs', $mc_minijs );
-			update_option( 'mc_caljs', $mc_caljs );
-			update_option( 'mc_ajaxjs', $mc_ajaxjs );
-		}
 
 		$mc_show_js = ( '' === $_POST['mc_show_js'] ) ? '' : $_POST['mc_show_js'];
 		update_option( 'mc_show_js', $mc_show_js );
 		mc_show_notice( __( 'Behavior Settings saved', 'my-calendar' ) );
 	}
-
-	$mc_listjs  = stripcslashes( get_option( 'mc_listjs' ) );
-	$mc_caljs   = stripcslashes( get_option( 'mc_caljs' ) );
-	$mc_minijs  = stripcslashes( get_option( 'mc_minijs' ) );
-	$mc_ajaxjs  = stripcslashes( get_option( 'mc_ajaxjs' ) );
-	$mc_show_js = stripcslashes( get_option( 'mc_show_js' ) );
+	$mc_show_js = get_option( 'mc_show_js' );
 	?>
 	<div class="wrap my-calendar-admin">
 		<h1><?php _e( 'My Calendar Scripting', 'my-calendar' ); ?></h1>
@@ -68,14 +52,19 @@ function my_calendar_behaviors_edit() {
 								<div>
 									<input type="hidden" name="_wpnonce" value="<?php echo wp_create_nonce( 'my-calendar-nonce' ); ?>"/>
 								</div>
+								<?php
+								if ( get_option( 'mc_use_custom_js' ) === '1' ) {
+									?>
 								<p>
 									<input type="checkbox" name="mc_use_custom_js" id="mc_use_custom_js" <?php mc_is_checked( 'mc_use_custom_js', 1 ); ?> />
 									<label for="mc_use_custom_js"><?php _e( 'Use Custom JS', 'my-calendar' ); ?></label>
 								</p>
-
+									<?php
+								}
+								?>
 								<p>
 									<label for="mc_show_js"><?php _e( 'Insert scripts on these pages (comma separated post IDs)', 'my-calendar' ); ?></label>
-									<input type="text" id="mc_show_js" name="mc_show_js" value="<?php echo esc_attr( $mc_show_js ); ?>"/>
+									<input type="text" id="mc_show_js" name="mc_show_js" value="<?php echo esc_attr( stripslashes( $mc_show_js ) ); ?>"/>
 								</p>
 
 								<div class='controls'>
@@ -97,21 +86,9 @@ function my_calendar_behaviors_edit() {
 											<label for="ajax_js"><?php _e( 'Disable AJAX', 'my-calendar' ); ?></label></li>
 									</ul>
 								</div>
-								<?php if ( get_option( 'mc_use_custom_js' ) === '1' ) { ?>
-									<p>
-										<label for="calendar-js"><?php _e( 'Calendar Behaviors: Grid View', 'my-calendar' ); ?></label><br/><textarea id="calendar-js" name="mc_caljs" rows="12" cols="80"><?php echo esc_textarea( $mc_caljs ); ?></textarea>
-									</p>
-									<p>
-										<label for="list-js"><?php _e( 'Calendar Behaviors: List View', 'my-calendar' ); ?></label><br/><textarea id="list-js" name="mc_listjs" rows="12" cols="80"><?php echo esc_textarea( $mc_listjs ); ?></textarea>
-									</p>
-									<p>
-										<label for="mini-js"><?php _e( 'Calendar Behaviors: Mini Calendar View', 'my-calendar' ); ?></label><br/><textarea id="mini-js" name="mc_minijs" rows="12" cols="80"><?php echo esc_textarea( $mc_minijs ); ?></textarea>
-									</p>
-									<p>
-										<label for="ajax-js"><?php _e( 'Calendar Behaviors: AJAX', 'my-calendar' ); ?></label><br/>
-										<textarea id="ajax-js" name="mc_ajaxjs" rows="12" cols="80"><?php echo esc_textarea( $mc_ajaxjs ); ?></textarea>
-									</p>
-								<?php } ?>
+								<?php if ( get_option( 'mc_use_custom_js' ) === '1' ) {
+									echo wp_kses_post( '<p>' . __( 'The mechanisms for adding custom JS were removed in My Calendar 3.3.0. The output for these scripts will be disabled in My Calendar 3.4.0.', 'my-calendar' ) . '</p>' ); 
+								} ?>
 								<p>
 									<input type="submit" name="mc-js-save" class="button-primary" value="<?php _e( 'Save', 'my-calendar' ); ?>"/>
 								</p>
