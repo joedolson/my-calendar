37479df4d692821346b5af7c0cf73c15fe7941df
Joe Dolson <design@joedolson.com>
2021-10-11 10:55:09 -0500

    Add test for sanitizing

---
+++ tests/test-calendar.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Class Tests_My_Calendar_General
+ *
+ * @package Sample_Plugin
+ */
+
+/**
+ * Sample test case.
+ */
+class Tests_My_Calendar_General extends WP_UnitTestCase {
+	/**
+	 * Verify that output of My Calendar is unchanged after sanitizing.
+	 *
+	 * Fails if attributes or elements not represented in kses filters.
+	 */
+	function test_my_calendar_sanitized_output() {
+		$output    = my_calendar( array() );
+		$sanitized = wp_kses( $output, mc_kses_elements() );
+		
+		$this->assertSame( $output, $sanitized );
+	}
+}
