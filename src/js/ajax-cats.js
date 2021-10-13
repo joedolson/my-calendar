71199e511279a600983d63ad317d175ed7ea8619
Joe Dolson <design@joedolson.com>
2021-10-13 17:55:23 -0500

    Insert new category into category list before new category button.
    Fixes #289

--- src/js/ajax-cats.js
+++ src/js/ajax-cats.js
@@ -12,7 +12,7 @@
 					if ( response.success == 1 ) {
 						var category_id = response.category_id;
 						$( '#event_category_name' ).val( '' );
-						$( '.categories .checkboxes' ).append( '<li class="new"><input type="checkbox" name="event_category[]" id="mc_cat_' + category_id + '" value="' + category_id + '" checked> <label for="mc_cat_' + category_id + '">' + category_name + '</label></li>' );
+						$( '<li class="new"><input type="checkbox" name="event_category[]" id="mc_cat_' + category_id + '" value="' + category_id + '" checked> <label for="mc_cat_' + category_id + '">' + category_name + '</label></li>' ).insertBefore( '.categories .event-new-category' );
 					}
 					console.log( response );
 				}, "json" );
