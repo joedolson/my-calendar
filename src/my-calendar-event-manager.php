7a0e1eafe7013c4f4b944f57dd6285f5ec9a0639
Joe Dolson <design@joedolson.com>
2021-11-11 14:16:23 -0600

    Warning thrown when event_begin not set

--- src/my-calendar-event-manager.php
+++ src/my-calendar-event-manager.php
@@ -148,8 +148,10 @@ function mc_add_post_meta_data( $post_id, $post, $data, $event_id ) {
 	update_post_meta( $post_id, '_mc_event_desc', $description );
 	update_post_meta( $post_id, '_mc_event_image', $image );
 	// This is only used by My Tickets, so only the first date occurrence is required.
-	$event_date = ( is_array( $data['event_begin'] ) ) ? $data['event_begin'][0] : $data['event_begin'];
-	update_post_meta( $post_id, '_mc_event_date', strtotime( $event_date ) );
+	if ( isset( $data['event_begin'] ) ) {
+		$event_date = ( is_array( $data['event_begin'] ) ) ? $data['event_begin'][0] : $data['event_begin'];
+		update_post_meta( $post_id, '_mc_event_date', strtotime( $event_date ) );
+	}
 	$location_id = ( isset( $post['location_preset'] ) ) ? (int) $post['location_preset'] : false;
 	if ( $location_id ) { // only change location ID if dropdown set.
 		update_post_meta( $post_id, '_mc_event_location', $location_id );
