055773c8d2d6b3e14b03cbe9809c56c0e6710e95
Joe Dolson <design@joedolson.com>
2021-11-24 16:05:34 -0600

    Don't hide button on final item.

--- src/my-calendar-settings.php
+++ src/my-calendar-settings.php
@@ -846,17 +846,13 @@ function mc_remote_db() {
 						$i      = 1;
 						$count  = count( $missed );
 						foreach ( $missed as $k => $v ) {
-							if ( $i !== $count ) {
-								// Translators: control to move down.
-								$down_label = sprintf( __( 'Move %s Down', 'my-calendar' ), $k );
-								// Translators: control to move up.
-								$up_label = sprintf( __( 'Move %s Up', 'my-calendar' ), $k );
-								// Translators: control to hide.
-								$hide_label = sprintf( __( 'Show %s', 'my-calendar' ), $k );
-								$buttons    = "<button class='up' type='button'><i class='dashicons dashicons-arrow-up' aria-hidden='true'></i><span class='screen-reader-text'>" . $up_label . "</span></button> <button class='down' type='button'><i class='dashicons dashicons-arrow-down' aria-hidden='true'></i><span class='screen-reader-text'>" . $down_label . "</span></button> <button class='hide' type='button'><i class='dashicons dashicons-hidden' aria-hidden='true'></i><span class='screen-reader-text'>" . $hide_label . '</span></button>';
-							} else {
-								$buttons = "<button class='up' type='button'><i class='dashicons dashicons-arrow-up' aria-hidden='true'></i><span class='screen-reader-text'>" . $up_label . "</span></button> <button class='hide' type='button'><i class='dashicons dashicons-hidden' aria-hidden='true'></i><span class='screen-reader-text'>" . $hide_label . '</span></button>';
-							}
+							// Translators: control to move down.
+							$down_label = sprintf( __( 'Move %s Down', 'my-calendar' ), $k );
+							// Translators: control to move up.
+							$up_label = sprintf( __( 'Move %s Up', 'my-calendar' ), $k );
+							// Translators: control to hide.
+							$hide_label = sprintf( __( 'Show %s', 'my-calendar' ), $k );
+							$buttons    = "<button class='up' type='button'><i class='dashicons dashicons-arrow-up' aria-hidden='true'></i><span class='screen-reader-text'>" . $up_label . "</span></button> <button class='down' type='button'><i class='dashicons dashicons-arrow-down' aria-hidden='true'></i><span class='screen-reader-text'>" . $down_label . "</span></button> <button class='hide' type='button'><i class='dashicons dashicons-hidden' aria-hidden='true'></i><span class='screen-reader-text'>" . $hide_label . '</span></button>';
 							$buttons = "<div class='mc-buttons'>$buttons</div>";
 							echo wp_kses( "<li class='ui-state-default mc-$k mc-hidden'>$buttons <code>$k</code> $v <input type='hidden' name='mc_nav[]' value='$k' disabled /></li>", mc_kses_elements() );
 							$i ++;
