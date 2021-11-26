be83660f9248b737b1497182b543dbc9c2cdc073
Joe Dolson <design@joedolson.com>
2021-11-26 14:15:02 -0600

    Add country to location search

--- src/my-calendar-location-manager.php
+++ src/my-calendar-location-manager.php
@@ -184,10 +184,10 @@ function mc_manage_locations() {
 		if ( '' !== $query ) {
 			if ( 'MyISAM' === $db_type ) {
 				$query  = esc_sql( $query ); // Prepare query.
-				$search = ' WHERE MATCH(' . apply_filters( 'mc_search_fields', 'location_label,location_city,location_state,location_region,location_street,location_street2,location_phone' ) . ") AGAINST ( '$query' IN BOOLEAN MODE ) ";
+				$search = ' WHERE MATCH(' . apply_filters( 'mc_search_fields', 'location_label,location_city,location_state,location_region,location_country,location_street,location_street2,location_phone' ) . ") AGAINST ( '$query' IN BOOLEAN MODE ) ";
 			} else {
 				$query  = esc_sql( $query ); // Prepare query.
-				$search = " WHERE location_label LIKE '%$query%' OR location_city LIKE '%$query%' OR location_state LIKE '%$query%' OR location_region LIKE '%$query%' OR location_street LIKE '%$query%' OR location_street2 LIKE '%$query%' OR location_phone LIKE '%$query%' ";
+				$search = " WHERE location_label LIKE '%$query%' OR location_city LIKE '%$query%' OR location_state LIKE '%$query%' OR location_region LIKE '%$query%' OR location_country LIKE '%$query%' OR location_street LIKE '%$query%' OR location_street2 LIKE '%$query%' OR location_phone LIKE '%$query%' ";
 			}
 		} else {
 			$search = '';
