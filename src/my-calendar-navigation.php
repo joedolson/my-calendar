3b88cbe58030a5abfee4564355da2b86445c9238
Joe Dolson <design@joedolson.com>
2021-11-29 12:20:49 -0600

    More line breaks for code formatting.

--- src/my-calendar-navigation.php
+++ src/my-calendar-navigation.php
@@ -207,11 +207,11 @@ function mc_generate_calendar_nav( $params, $cat, $start_of_week, $show_months, 
 	}
 
 	if ( '' !== $mc_topnav ) {
-		$mc_topnav = PHP_EOL . '<nav aria-label="' . __( 'Calendar (top)', 'my-calendar' ) . '"><div class="my-calendar-header">' . $mc_topnav . '</div></nav>' . PHP_EOL;
+		$mc_topnav = PHP_EOL . '<nav aria-label="' . __( 'Calendar (top)', 'my-calendar' ) . '">' . PHP_EOL . '<div class="my-calendar-header">' . $mc_topnav . '</div>' . PHP_EOL . '</nav>' . PHP_EOL;
 	}
 
 	if ( '' !== $mc_bottomnav ) {
-		$mc_bottomnav = PHP_EOL . '<nav aria-label="' . __( 'Calendar (bottom)', 'my-calendar' ) . '"><div class="mc_bottomnav my-calendar-footer">' . $mc_bottomnav . '</div></nav>' . PHP_EOL;
+		$mc_bottomnav = PHP_EOL . '<nav aria-label="' . __( 'Calendar (bottom)', 'my-calendar' ) . '">' . PHP_EOL . '<div class="mc_bottomnav my-calendar-footer">' . $mc_bottomnav . '</div>' . PHP_EOL . '</nav>' . PHP_EOL;
 	}
 
 	if ( $site ) {
