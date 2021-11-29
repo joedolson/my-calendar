bc89af96e01464c5337494efdc4b4db38f0ba301
Joe Dolson <design@joedolson.com>
2021-11-29 15:57:32 -0600

    Implement masking on grid popups.

--- src/js/mc-grid.js
+++ src/js/mc-grid.js
@@ -2,11 +2,15 @@
 	'use strict';
 	$(function () {
 		$('.calendar-event').children().not('.event-title,.screen-reader-text').hide();
-
+		var mask = document.createElement( 'div' );
+		mask.classList.add( 'my-calendar-mask' );
+		var body = document.querySelector( 'body' );
+		body.insertAdjacentElement( 'beforeend', mask );
 		$(document).on('click', '.calendar-event .event-title .open',
 			function (e) {
 				e.preventDefault();
 				var current_date = $(this).parents( '.mc-event' ).children();
+				mask.classList.add( 'mc-mask-active' );
 
 				$(this).closest( '.mc-main' ).toggleClass( 'grid-open' );
 				$(this).parents( '.mc-event' ).children().not('.event-title').toggle().attr('tabindex', '-1');
@@ -23,6 +27,7 @@
 
 		$(document).on('click', '.calendar-event .close',
 			function (e) {
+				mask.classList.remove( 'mc-mask-active' );
 				e.preventDefault();
 				$(this).closest( '.mc-main' ).removeClass( 'grid-open' );
 				$(this).closest('.mc-event').find('.event-title a').trigger( 'focus' );
@@ -32,6 +37,7 @@
 		$(document).on( 'keydown', function(e) {
 			var keycode = ( e.keyCode ? e.keyCode : e.which );
 			if ( keycode == 27 ) {
+				mask.classList.remove( 'mc-mask-active' );
 				$( '.mc-main ').removeClass( 'grid-open' );
 				$( '.calendar-event div.details' ).hide();
 			}
