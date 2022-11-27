3c6c53e0a51c062559678b6bd910bd42b75149e1
Joe Dolson <design@joedolson.com>
2022-11-27 12:43:31 -0600

    Init stub for running locally

---
+++ init.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * My Calendar, Accessible Events Manager for WordPress
+ *
+ * @package     MyCalendar
+ * @author      Joe Dolson
+ * @copyright   2009-2022 Joe Dolson
+ * @license     GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: My Calendar
+ * Plugin URI:  http://www.joedolson.com/my-calendar/
+ * Description: Accessible WordPress event calendar plugin. Show events from multiple calendars on pages, in posts, or in widgets.
+ * Author:      Joseph C Dolson
+ * Author URI:  http://www.joedolson.com
+ * Text Domain: my-calendar
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/license/gpl-2.0.txt
+ * Domain Path: lang
+ * Version:     3.3.24.1
+ */
+
+/*
+	Copyright 2009-2022  Joe Dolson (email : joe@joedolson.com)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+include( dirname( __FILE__ ) . '/src/my-calendar.php' );
