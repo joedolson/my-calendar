843025ab8588fc8a9fdf9f4c55ab135a19441774
Joe Dolson <design@joedolson.com>
2022-12-14 17:40:48 -0600

    Add template loader for next event

---
+++ src/mc-templates/event-next.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template: Single Event, Upcoming events view.
+ *
+ * @category Templates
+ * @package  My Calendar
+ * @author   Joe Dolson
+ * @license  GPLv2 or later
+ * @link     https://www.joedolson.com/my-calendar/
+ */
+
+?>
+<li class="<?php mc_event_classes( $event['event'] ); ?>"><strong class="mc-title"><?php mc_template_tag( $event, 'title' ); ?></strong> - <?php mc_template_tag( $event, 'datetime' ); ?>
+<?php mc_template_tag( $event, 'excerpt' ); ?></li>
