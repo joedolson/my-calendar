0df95e53a0ef076df9b20256cde65e0c93726fef
Joe Dolson <design@joedolson.com>
2023-01-17 15:01:33 -0600

    Escape early if `mime_content_type` not available.

--- src/my-calendar-categories.php
+++ src/my-calendar-categories.php
@@ -38,6 +38,9 @@ function mc_update_category( $field, $data, $category ) {
  * @return array images in directory.
  */
 function mc_directory_list( $directory ) {
+	if ( ! function_exists( 'mime_content_type' ) ) {
+		return array();
+	}
 	if ( ! file_exists( $directory ) ) {
 		return array();
 	}
