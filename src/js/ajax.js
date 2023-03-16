abdb177300adbe9eb254215095d7d15e76c984a4
Joe Dolson <design@joedolson.com>
2023-03-16 16:06:11 -0500

    Collapse ajax-cats and ajax

--- src/js/ajax.js
+++ src/js/ajax.js
@@ -122,4 +122,24 @@
 			});
 		}
 	});
+	$(function () {
+		$( 'button.add-category' ).on( 'click', function() {
+			var category_name = $( '#event_category_name' ).val();
+			if ( '' !== category_name ) {
+				var data    = {
+					'action': mc_cats.action,
+					'category_name': category_name,
+					'security': mc_cats.security
+				};
+				$.post( ajaxurl, data, function (response) {
+					if ( response.success == 1 ) {
+						var category_id = response.category_id;
+						$( '#event_category_name' ).val( '' );
+						$( '<li class="new"><input type="checkbox" name="event_category[]" id="mc_cat_' + category_id + '" value="' + category_id + '" checked> <label for="mc_cat_' + category_id + '">' + category_name + '</label></li>' ).insertBefore( '.categories .event-new-category' );
+					}
+				}, "json" );
+			}
+		});
+
+	});
 }(jQuery));
