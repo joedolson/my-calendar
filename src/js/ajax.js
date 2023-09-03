32f7f9ce5a85b23e3bff5e2eef6e88d8506755e3
Joe Dolson <design@joedolson.com>
2023-09-03 14:10:55 -0500

    Add new category to primary dropdown on creation
    Fixes #564

--- src/js/ajax.js
+++ src/js/ajax.js
@@ -136,6 +136,8 @@
 						var category_id = response.category_id;
 						$( '#event_category_name' ).val( '' );
 						$( '<li class="new"><input type="checkbox" name="event_category[]" id="mc_cat_' + category_id + '" value="' + category_id + '" checked> <label for="mc_cat_' + category_id + '">' + category_name + '</label></li>' ).insertBefore( '.categories .event-new-category' );
+						var primary = $( '#e_category' );
+						primary.append( '<option value="' + category_id + '">' + category_name + '</option>' );
 					}
 				}, "json" );
 			}
