f0b83df6e1ebfaff1db3ae7cab064b1063a60e9d
Joe Dolson <design@joedolson.com>
2023-10-25 15:21:03 -0500

    Add card template file.

---
+++ src/mc-templates/event-card.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template: Single Event, List view.
+ *
+ * @category Templates
+ * @package  My Calendar
+ * @author   Joe Dolson
+ * @license  GPLv2 or later
+ * @link     https://www.joedolson.com/my-calendar/
+ */
+
+?>
+<div class="mc-event mc-single mc-list">
+	<?php mc_template_tag( $data['event'], 'image' ); ?>
+	<h2 class="mc-title"><?php mc_template_tag( $data['event'], 'title' ); ?></h2>
+	<?php mc_template_tag( $data['event'], 'datetime' ); ?>
+	<?php mc_template_tag( $data['event'], 'description' ); ?>
+</div>
