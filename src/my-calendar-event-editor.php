865f072dc8538f2a7852cdb185855de434f1c533
Joe Dolson <design@joedolson.com>
2023-11-12 17:02:45 -0600

    Fix incorrectly formatted doc

--- src/my-calendar-event-editor.php
+++ src/my-calendar-event-editor.php
@@ -1908,32 +1908,30 @@ function mc_event_location_dropdown_block( $data ) {
  * @return array
  */
 function mc_event_access() {
+	$choices = array(
+		'1'  => __( 'Audio Description', 'my-calendar' ),
+		'2'  => __( 'ASL Interpretation', 'my-calendar' ),
+		'3'  => __( 'ASL Interpretation with voicing', 'my-calendar' ),
+		'4'  => __( 'Deaf-Blind ASL', 'my-calendar' ),
+		'5'  => __( 'Real-time Captioning', 'my-calendar' ),
+		'6'  => __( 'Scripted Captioning', 'my-calendar' ),
+		'7'  => __( 'Assisted Listening Devices', 'my-calendar' ),
+		'8'  => __( 'Tactile/Touch Tour', 'my-calendar' ),
+		'9'  => __( 'Braille Playbill', 'my-calendar' ),
+		'10' => __( 'Large Print Playbill', 'my-calendar' ),
+		'11' => __( 'Sensory Friendly', 'my-calendar' ),
+		'12' => __( 'Other', 'my-calendar' ),
+	);
 	/**
 	 * Filter available event accessibility options.
 	 *
 	 * @hook mc_event_access_choices
 	 *
-	 * @param {array} Indexed array of choices. Events store only the index.
+	 * @param {array} $choices Indexed array of choices. Events store only the index.
 	 *
 	 * @return {array}
 	 */
-	$event_access = apply_filters(
-		'mc_event_access_choices',
-		array(
-			'1'  => __( 'Audio Description', 'my-calendar' ),
-			'2'  => __( 'ASL Interpretation', 'my-calendar' ),
-			'3'  => __( 'ASL Interpretation with voicing', 'my-calendar' ),
-			'4'  => __( 'Deaf-Blind ASL', 'my-calendar' ),
-			'5'  => __( 'Real-time Captioning', 'my-calendar' ),
-			'6'  => __( 'Scripted Captioning', 'my-calendar' ),
-			'7'  => __( 'Assisted Listening Devices', 'my-calendar' ),
-			'8'  => __( 'Tactile/Touch Tour', 'my-calendar' ),
-			'9'  => __( 'Braille Playbill', 'my-calendar' ),
-			'10' => __( 'Large Print Playbill', 'my-calendar' ),
-			'11' => __( 'Sensory Friendly', 'my-calendar' ),
-			'12' => __( 'Other', 'my-calendar' ),
-		)
-	);
+	$event_access = apply_filters( 'mc_event_access_choices', $choices );
 
 	return $event_access;
 }
