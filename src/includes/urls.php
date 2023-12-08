464e7a53cf592f66d3d6f1f1d1e67904a304dfce
Joe Dolson <design@joedolson.com>
2023-12-08 11:15:29 -0600

    Screwed up nesting

--- src/includes/urls.php
+++ src/includes/urls.php
@@ -99,7 +99,7 @@ function mc_url_in_loop( $url ) {
  */
 function mc_build_mini_url( $start, $category, $events, $args, $date ) {
 	$open_day_uri = mc_get_option( 'open_day_uri' );
-	$mini_uri     = ( _mc_is_url( mc_get_option( 'mini_uri' ) ) ) ? mc_get_option( 'mini_uri' ) : mc_get_uri( reset( $events );
+	$mini_uri     = ( _mc_is_url( mc_get_option( 'mini_uri' ) ) ) ? mc_get_option( 'mini_uri' ) : mc_get_uri( reset( $events ) );
 	/**
 	 * Filter the URI used to link days in the mini calendar.
 	 *
@@ -110,7 +110,7 @@ function mc_build_mini_url( $start, $category, $events, $args, $date ) {
 	 *
 	 * @return string URL.
 	 */
-	$mini_uri     = apply_filters( 'mc_modify_day_uri', $mini_uri, $args ) );
+	$mini_uri     = apply_filters( 'mc_modify_day_uri', $mini_uri, $args );
 
 	if ( 'true' === $open_day_uri || 'false' === $open_day_uri ) {
 		// Yes, this is weird. it's from some old settings...
