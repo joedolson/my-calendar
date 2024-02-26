06526a24266cffae8c930f5a28308c11d26a0812
Joe Dolson <design@joedolson.com>
2024-02-26 10:32:30 -0600

    Update variable declarations

--- src/js/admin.js
+++ src/js/admin.js
@@ -1,15 +1,15 @@
-var clipboard = new ClipboardJS('.mc-copy-to-clipboard');
+const clipboard = new ClipboardJS('.mc-copy-to-clipboard');
 clipboard.on( 'success', function(e) {
-	var parent   = e.trigger.parentNode;
-	var response = parent.querySelector( '.mc-notice-copied' );
-	var text     = response.textContent;
+	let parent   = e.trigger.parentNode;
+	let response = parent.querySelector( '.mc-notice-copied' );
+	let text     = response.textContent;
 	wp.a11y.speak( text );
 	response.classList.add( 'visible' );
 });
 
 window.addEventListener( 'beforeunload', function(e) {
-	var unsubmitted = document.getElementById( 'mc_unsubmitted' );
-	var hold        = ( typeof( unsubmitted ) != 'undefined' && unsubmitted != null ) ? true : false;
+	let unsubmitted = document.getElementById( 'mc_unsubmitted' );
+	let hold        = ( typeof( unsubmitted ) != 'undefined' && unsubmitted != null ) ? true : false;
 	if ( hold ) {
 		// following two lines will cause the browser to ask the user if they
 		// want to leave. The text of this dialog is controlled by the browser.
