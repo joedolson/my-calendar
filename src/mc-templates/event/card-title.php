45a7edbe8ef7de204068a7b4fe82d26994bd90ed
Joe Dolson <design@joedolson.com>
2024-03-03 18:11:02 -0600

    Really?

--- src/mc-templates/event/card-title.php
+++ src/mc-templates/event/card-title.php
@@ -10,7 +10,7 @@
  */
 
 $title_template = mc_get_template( 'title_card' );
-if (  mc_template_settings( 'title_card' ) !== $title_template ) {
+if ( mc_template_settings( 'title_card' ) !== $title_template ) {
 	// If the title template has been modified, use that.
 	echo wp_kses_post( mc_draw_template( $data, $title_template ) );
 } else {
