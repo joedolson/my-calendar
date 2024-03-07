399125b86c4286473d443d3df62f056f15020cc8
Joe Dolson <design@joedolson.com>
2024-03-07 16:57:10 -0600

    Add separate template for search results; fix search output

---
+++ src/mc-templates/event/search.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template: Single Event, Search results events view.
+ *
+ * @category Templates
+ * @package  My Calendar
+ * @author   Joe Dolson
+ * @license  GPLv2 or later
+ * @link     https://www.joedolson.com/my-calendar/
+ */
+
+?>
+<li class="<?php mc_event_classes( $data->event, 'search' ); ?>"><?php mc_template( $data->tags, $data->template, 'list' ); ?></li>
