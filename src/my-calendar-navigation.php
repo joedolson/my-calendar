9d54ad24065e4ec9ab7ea50f1799765c6e9f76e1
Joe Dolson <design@joedolson.com>
2024-12-05 17:06:45 -0600

    "my-calendar-ical" is not the name of that feed.

--- src/my-calendar-navigation.php
+++ src/my-calendar-navigation.php
@@ -479,7 +479,7 @@ function mc_sub_links() {
 
 	$google = str_replace( $search, $replace, get_feed_link( 'my-calendar-google' ) );
 	$google = add_query_arg( 'cid', $google, 'https://www.google.com/calendar/render' );
-	$ical   = str_replace( $search, $replace, get_feed_link( 'my-calendar-ical' ) );
+	$ical   = str_replace( $search, $replace, get_feed_link( 'my-calendar-ics' ) );
 
 	$sub_google = "<li class='ics google'><a href='" . esc_url( $google ) . "' rel='nofollow'><span class='mc-icon' aria-hidden='true'></span>" . __( '<span class="maybe-hide">Subscribe in </span>Google', 'my-calendar' ) . '</a></li>';
 	$sub_ical   = "<li class='ics ical'><a href='" . esc_url( $ical ) . "' rel='nofollow'><span class='mc-icon' aria-hidden='true'></span>" . __( '<span class="maybe-hide">Subscribe in </span>iCal', 'my-calendar' ) . '</a></li>';
