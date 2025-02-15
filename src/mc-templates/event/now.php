94729c3d06d774b7b841ac35ddbd4e0a13b4e3c4
Joe Dolson <design@joedolson.com>
2025-02-14 18:12:09 -0600

    These shortcodes don't return a list.

--- src/mc-templates/event/now.php
+++ src/mc-templates/event/now.php
@@ -10,4 +10,4 @@
  */
 
 ?>
-<li class="mc-events <?php mc_event_classes( $data->event, 'now', array( $data->class ) ); ?>"><?php mc_template( $data->tags, $data->template, 'list' ); ?></li>
+<div class="mc-events <?php mc_event_classes( $data->event, 'now', array( $data->class ) ); ?>"><?php mc_template( $data->tags, $data->template, 'list' ); ?></div>
