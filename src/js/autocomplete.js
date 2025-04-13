fb1c88c74168ee756592427b257a04353fc78fa9
Joe Dolson <design@joedolson.com>
2025-04-13 11:43:51 -0500

    Some style polishing

--- src/js/autocomplete.js
+++ src/js/autocomplete.js
@@ -1,70 +1,76 @@
 (function( $ ) { 'use strict';
-	/* https://autocomplete.trevoreyre.com/#/javascript-component?id=getresultvalue */
-	if ( typeof( mclocations ) !== 'undefined' ) {
-		new Autocomplete( '#mc-locations-autocomplete', {
-			search: input => {
-				const url = mclocations.ajaxurl;
-				return new Promise( resolve => {
-					if (input.length < 3) {
-						return resolve([])
-					}
+	let locationsHandler = document.getElementById( 'mc-locations-autocomplete' );
+	if ( locationsHandler ) {
+		/* https://autocomplete.trevoreyre.com/#/javascript-component?id=getresultvalue */
+		if ( typeof( mclocations ) !== 'undefined' ) {
+			new Autocomplete( '#mc-locations-autocomplete', {
+				search: input => {
+					const url = mclocations.ajaxurl;
+					return new Promise( resolve => {
+						if (input.length < 3) {
+							return resolve([])
+						}
 
-					const data = new FormData();
-					data.append( 'action', mclocations.action );
-					data.append( 'security', mclocations.security );
-					data.append( 'data', input );
-					const response = fetch(url, {
-						method: 'POST',
-						credentials: 'same-origin',
-						body: data
-					}).then(response => response.json())
-					.then(data => {
-						resolve(data.response)
+						const data = new FormData();
+						data.append( 'action', mclocations.action );
+						data.append( 'security', mclocations.security );
+						data.append( 'data', input );
+						const response = fetch(url, {
+							method: 'POST',
+							credentials: 'same-origin',
+							body: data
+						}).then(response => response.json())
+						.then(data => {
+							resolve(data.response)
+						})
 					})
-				})
-			},
-			onSubmit: result => {
-				let location_field = document.getElementById( 'mc_event_location_value' );
+				},
+				onSubmit: result => {
+					let location_field = document.getElementById( 'mc_event_location_value' );
 
-				location_field.value = result.location_id;
-				$( location_field ).trigger( 'change' );
-			},
-			getResultValue: result => ( '' !== result.location_label ) ? result.location_label : '#' + result.location_id
-		});
+					location_field.value = result.location_id;
+					$( location_field ).trigger( 'change' );
+				},
+				getResultValue: result => ( '' !== result.location_label ) ? result.location_label : '#' + result.location_id
+			});
+		}
 	}
 
 	if ( typeof( mcpages ) !== 'undefined' ) {
-		/* https://autocomplete.trevoreyre.com/#/javascript-component?id=getresultvalue */
-		new Autocomplete( '#mc-pages-autocomplete', {
-			search: input => {
-				const url = mcpages.ajaxurl;
-				return new Promise( resolve => {
-					if (input.length < 3) {
-						return resolve([])
-					}
+		let pagesHandler = document.getElementById( 'mc-pages-autocomplete' );
+		if ( pagesHandler ) {
+			/* https://autocomplete.trevoreyre.com/#/javascript-component?id=getresultvalue */
+			new Autocomplete( '#mc-pages-autocomplete', {
+				search: input => {
+					const url = mcpages.ajaxurl;
+					return new Promise( resolve => {
+						if (input.length < 3) {
+							return resolve([])
+						}
 
-					const data = new FormData();
-					data.append( 'action', mcpages.action );
-					data.append( 'security', mcpages.security );
-					data.append( 'data', input );
-					const response = fetch(url, {
-						method: 'POST',
-						credentials: 'same-origin',
-						body: data
-					}).then(response => response.json())
-					.then(data => {
-						resolve(data.response)
+						const data = new FormData();
+						data.append( 'action', mcpages.action );
+						data.append( 'security', mcpages.security );
+						data.append( 'data', input );
+						const response = fetch(url, {
+							method: 'POST',
+							credentials: 'same-origin',
+							body: data
+						}).then(response => response.json())
+						.then(data => {
+							resolve(data.response)
+						})
 					})
-				})
-			},
-			onSubmit: result => {
-				let pages_field = document.getElementById( 'mc_uri_id' );
+				},
+				onSubmit: result => {
+					let pages_field = document.getElementById( 'mc_uri_id' );
 
-				pages_field.value = result.post_id;
-				$( pages_field ).trigger( 'change' );
-			},
-			getResultValue: result => ( '' !== result.post_title ) ? result.post_title : '#' + result.post_id
-		});
+					pages_field.value = result.post_id;
+					$( pages_field ).trigger( 'change' );
+				},
+				getResultValue: result => ( '' !== result.post_title ) ? result.post_title : '#' + result.post_id
+			});
+		}
 	}
 
 	if ( typeof( mcicons ) !== 'undefined' ) {
