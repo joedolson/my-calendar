5a904148e9d4e27a02f2465c0913fe9a5f83f5ee
Joe Dolson <design@joedolson.com>
2025-07-10 18:38:07 -0500

    Update my-calendar-output.php

--- src/my-calendar-output.php
+++ src/my-calendar-output.php
@@ -996,7 +996,7 @@ function mc_edit_panel( $html, $event, $type, $time ) {
 			$del_group  = false;
 		}
 		if ( 'S' === $recur || ( ! $edit_group && ! $del_group ) ) {
-			$edit .= ( $edit_url ) ? "<li><a href='" . esc_url( $edit_url ) . "' class='edit'>" . __( 'Edit', 'my-calendar' ) . '</a></li>' : ''; 
+			$edit .= ( $edit_url ) ? "<li><a href='" . esc_url( $edit_url ) . "' class='edit'>" . __( 'Edit', 'my-calendar' ) . '</a></li>' : '';
 			$edit .= ( $delete_url ) ? "<li><a href='" . esc_url( $delete_url ) . "' class='delete'>" . __( 'Delete', 'my-calendar' ) . '</a></li>' : '';
 			$edit .= $groupedit;
 		} else {
