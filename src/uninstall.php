a93685cd8a25178733467372723ed9dac80d5594
Joe Dolson <design@joedolson.com>
2026-01-09 17:00:56 -0600

    Delete version when deleting plugin.
    Not doing this means that repeat installations don't run installation routines, even if info has been dropped.

--- src/uninstall.php
+++ src/uninstall.php
@@ -74,5 +74,6 @@ if ( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	}
 
 	delete_option( 'mc_promotion_scheduled' );
+	delete_option( 'mc_version' );
 	add_option( 'mc_uninstalled', 'true' );
 }
