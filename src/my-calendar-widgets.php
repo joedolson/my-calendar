8eccf4ce9454d80bfd4f6c6db503935ac16a3a72
Stephen A. Bernhardt <sabernhardt@yahoo.com>
2026-02-17 20:21:14 -0600

    Update documentation for default `$header` and `$footer` in upcoming events widget

--- src/my-calendar-widgets.php
+++ src/my-calendar-widgets.php
@@ -237,7 +237,7 @@ function my_calendar_upcoming_events( $args ) {
 		}
 	}
 	/**
-	 * Replace the list header for upcoming events lists. Default value `<ul id='upcoming-events-$hash' class='mc-event-list upcoming-events'$lang>`.
+	 * Replace the list header for upcoming events lists. Default value `<div class='mc-event-list-container'><ul id='upcoming-events-$hash' class='mc-event-list upcoming-events'$lang>`.
 	 *
 	 * @hook mc_upcoming_events_header
 	 *
@@ -247,7 +247,7 @@ function my_calendar_upcoming_events( $args ) {
 	 */
 	$header = apply_filters( 'mc_upcoming_events_header', $header );
 	/**
-	 * Replace the list footer for upcoming events lists. Default value `</ul>`.
+	 * Replace the list footer for upcoming events lists. Default value `</ul></div>`.
 	 *
 	 * @hook mc_upcoming_events_footer
 	 *
