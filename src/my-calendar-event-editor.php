d98508c00a07c6ab5fdcd2a31f77660ff221a096
Joe Dolson <design@joedolson.com>
2026-05-19 12:28:39 -0500

    Bug fix: accidentally removed ability to set alternate event statuses.

--- src/my-calendar-event-editor.php
+++ src/my-calendar-event-editor.php
@@ -802,6 +802,12 @@ function my_calendar_save( $action, $output, $event_id = false ) {
 			} else {
 				// do an action using the $action and processed event data.
 				$new_event_status = ( current_user_can( 'mc_approve_events' ) ) ? 1 : 0;
+				if ( isset( $post['event_approved'] ) && $post['event_approved'] !== $new_event_status ) {
+					// mc_publish_events, mc_approve_events, and mc_manage_events all grant access to all published states.
+					if ( current_user_can( 'mc_publish_events' ) || current_user_can( 'mc_approve_events' ) || current_user_can( 'mc_manage_events' ) ) {
+						$new_event_status = absint( $post['event_approved'] );
+					}
+				}
 				if ( isset( $post['prev_event_status'] ) ) {
 					/**
 					 * Execute an action when an event changes status.
@@ -2392,6 +2398,12 @@ function mc_check_data( $action, $post, $i, $ignore_required = false ) {
 		$event_link   = ! empty( $post['event_link'] ) ? trim( $post['event_link'] ) : '';
 		$expires      = ! empty( $post['event_link_expires'] ) ? $post['event_link_expires'] : '0';
 		$approved     = ( current_user_can( 'mc_approve_events' ) ) ? 1 : 0;
+		if ( isset( $post['event_approved'] ) && $post['event_approved'] !== $approved ) {
+			// mc_publish_events, mc_approve_events, and mc_manage_events all grant access to all published states.
+			if ( current_user_can( 'mc_publish_events' ) || current_user_can( 'mc_approve_events' ) || current_user_can( 'mc_manage_events' ) ) {
+				$approved = absint( $post['event_approved'] );
+			}
+		}
 
 		$saved_location     = ! empty( $post['preset_location'] ) ? $post['preset_location'] : '';
 		$select_location    = ( ! empty( $post['location_preset'] ) ) ? $post['location_preset'] : '';
