5fe0fee7505c8b5dc8f1cf33b594375c248b37aa
Joe Dolson <design@joedolson.com>
2026-07-12 14:05:14 -0500

    Reset location count transient when location deleted

--- src/my-calendar-locations.php
+++ src/my-calendar-locations.php
@@ -471,6 +471,7 @@ function mc_delete_location( $location, $type = 'string' ) {
 		$value  = false;
 		$return = mc_show_error( __( 'Location could not be deleted', 'my-calendar' ), false );
 	}
+	delete_transient( 'mc_location_count' );
 	delete_transient( 'mc_location_' . $location );
 
 	return ( 'string' === $type ) ? $return : $value;
